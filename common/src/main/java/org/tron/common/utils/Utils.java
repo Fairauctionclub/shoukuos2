2214639b17a641f6fbd66febc1e0db35a381adb1
teamfairauction <94896055+teamfairauction@users.noreply.github.com>
2022-05-16 16:29:44 +0800

    Create Utils.java

---
+++ common/src/main/java/org/tron/common/utils/Utils.java
@@ -0,0 +1,41 @@
+package org.tron.common.utils;
+
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.charset.Charset;
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+public interface Utils {
+
+  SecureRandom random = new SecureRandom();
+
+  static SecureRandom getRandom() {
+    return random;
+  }
+
+  
+
+  static String align(String s, char fillChar, int targetLen, boolean alignRight) {
+    if (targetLen <= s.length()) {
+      return s;
+    }
+    String alignString = repeat("" + fillChar, targetLen - s.length());
+    return alignRight ? alignString + s : s + alignString;
+
+  }
+
+  static String repeat(String s, int n) {
+    if (s.length() == 1) {
+      byte[] bb = new byte[n];
+      Arrays.fill(bb, s.getBytes()[0]);
+      return new String(bb);
+    } else {
+      StringBuilder ret = new StringBuilder();
+      for (int i = 0; i < n; i++) {
+        ret.append(s);
+      }
+      return ret.toString();
+    }
+  }
+}
