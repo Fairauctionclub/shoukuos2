429296c6dd74475d274fbebfaca81c77cfa959ab
Fairauction <94896055+Fairauctionclub@users.noreply.github.com>
2022-08-24 16:42:44 +0800

    a

--- common/src/main/java/org/tron/common/utils/Time.java
+++ common/src/main/java/org/tron/common/utils/Time.java
@@ -5,10 +5,14 @@ import java.sql.Timestamp;
 public class Time {
 
   public static long getCurrentMillis() {
+    
     return System.currentTimeMillis();
+    
   }
 
   public static String getTimeString(long time) {
+    
     return new Timestamp(time).toString();
+    
   }
 }
